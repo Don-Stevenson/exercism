27bed2209f358af3a84b0df28812ae89248c06a0
Don-Stevenson <dsteven1@gmail.com>
2020-08-21 14:33:44 -0400

    solution to two fer challenge

---
+++ two-fer.js
@@ -0,0 +1,36 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+
+
+// # Two Fer
+
+// `Two-fer` or `2-fer` is short for two for one. One for you and one for me.
+
+// Given a name, return a string with the message:
+
+// ```text
+// One for X, one for me.
+// ```
+
+// Where X is the given name.
+
+// However, if the name is missing, return the string:
+
+// ```text
+// One for you, one for me.
+// ```
+
+// Here are some examples:
+
+// |Name    |String to return 
+// |:-------|:------------------
+// |Alice   |One for Alice, one for me. 
+// |Bob     |One for Bob, one for me.
+// |        |One for you, one for me.
+// |Zaphod  |One for Zaphod, one for me.
+
+
+export const twoFer = (name = "you") => `One for ${name}, one for me.`
