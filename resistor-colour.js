aee0ee0e349b3bace419525020d19f37bb04a448
Don-Stevenson <dsteven1@gmail.com>
2020-08-22 11:31:30 -0400

    add solution to resitor colour problem

---
+++ resistor-colour.js
@@ -0,0 +1,59 @@
+// # Resistor Color
+
+// Resistors have color coded bands, where each color maps to a number.
+//The first 2 bands of a resistor have a simple encoding scheme: each color maps to a single number.
+
+// These colors are encoded as follows:
+
+// - Black: 0
+// - Brown: 1
+// - Red: 2
+// - Orange: 3
+// - Yellow: 4
+// - Green: 5
+// - Blue: 6
+// - Violet: 7
+// - Grey: 8
+// - White: 9
+
+// The goal of this exercise is to create a way:
+// - to look up the numerical value associated with a particular color band
+// - to list the different band colors
+
+// Mnemonics map the colors to the numbers, that, when stored as an array,
+// happen to map to their index in the array: Better Be Right Or Your Great Big Values Go Wrong.
+
+// More information on the color encoding of resistors can be found in the [Electronic color code Wikipedia article](https://en.wikipedia.org/wiki/Electronic_color_code)
+
+export const colorCode = colour => {
+
+    // return COLORS.findIndex(colorCode(colour))
+  
+    const colourObj = {
+      black: 0,
+      brown: 1,
+      red: 2,
+      orange: 3,
+      yellow: 4,
+      green: 5,
+      blue: 6,
+      violet: 7,
+      grey: 8,
+      white: 9
+    };
+  
+    return colourObj[colour];
+  };
+  
+  export const COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white"
+  ];
