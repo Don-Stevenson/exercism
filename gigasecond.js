91de497090829d449827f31de3defa4a65ae8b9d
Don-Stevenson <dsteven1@gmail.com>
2020-08-27 19:15:04 -0400

    update comments

--- gigasecond.js
+++ gigasecond.js
@@ -12,7 +12,7 @@
 
 // It is possible to return a correct value for this exercise by mutating the solution function argument. 
 // Although there are legitimate use cases for mutating function arguments, this is usually undesirable, and in the case of this exercise, clearly unexpected. For this reason, the test suite has a test that fails in case the argument has been modified after the function execution.
-
+//
 const MILLI = 1000;
 const BILLION = 1e9;
 
