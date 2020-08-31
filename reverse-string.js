98f9802bb07812d90dc9d7f28597adee60b84b47
Don-Stevenson <dsteven1@gmail.com>
2020-08-31 18:46:16 -0400

    with if else statement

--- reverse-string.js
+++ reverse-string.js
@@ -3,5 +3,15 @@
 // For example:
 // input: "cool"
 // output: "looc"
+// const string = "hello";
+// console.log(string.substr(1), string.charAt(0));
 
-export const reverseString = str => (str === "") ? "" : reverseString(str.substr(1)) + str.charAt(0)
+const reverseString = str => {
+  if (str === "") return "";
+  else {
+    console.log(str);
+    return reverseString(str.substr(1)) + str.charAt(0);
+  }
+};
+
+console.log(reverseString("Here"));
