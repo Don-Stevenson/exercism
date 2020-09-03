4e67eda61ff4eb0db1bfa029430e6e7566be108a
Don-Stevenson <dsteven1@gmail.com>
2020-09-03 15:14:46 -0400

    refactor to ternary

--- reverse-string.js
+++ reverse-string.js
@@ -7,11 +7,7 @@
 // console.log(string.substr(1), string.charAt(0));
 
 const reverseString = str => {
-  if (str === "") return "";
-  else {
-    console.log(str);
-    return reverseString(str.substr(1)) + str.charAt(0);
-  }
+  return (str === "") ? "" : reverseString(str.substr(1)) + str.charAt(0);
 };
 
 console.log(reverseString("Here"));
