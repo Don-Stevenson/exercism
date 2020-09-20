2c00ef8a24805f8ea592f320d28d3dba1aba51e5
Don-Stevenson <dsteven1@gmail.com>
2020-09-19 21:42:36 -0400

    not working between

--- lessThanGreaterThan.js
+++ lessThanGreaterThan.js
@@ -1,5 +1,6 @@
 const between = (x) =>{
-    if (8>= x >=1) return "between"
+    if (1 <= x >=8) return "between 1 and 8" 
+    else if(20>= x >9) return "between 9 and 20"
     else return "not between"
 }
 
