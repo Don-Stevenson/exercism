893703a6a4fe946665578c3404a0c976753e3f93
Don-Stevenson <dsteven1@gmail.com>
2020-09-19 22:29:29 -0400

    update comments

--- lessThanGreaterThan.js
+++ lessThanGreaterThan.js
@@ -1,7 +1,10 @@
-const between = (x) =>{
-    if (1 <= x >=8) return "between 1 and 8" 
-    else if(20>= x >9) return "between 9 and 20"
+// testing out if statements between two values
+//
+
+const between = (x) => {
+    if (1 <= x <= 8 ) return "between 1 and 8" 
+    else if (9 < x <=20) return "between 9 and 20"
     else return "not between"
 }
 
-console.log(between(6))
+console.log(between(21))
