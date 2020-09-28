57754e5a9c06b1412fd1bf21652996c774f1728e
Don-Stevenson <dsteven1@gmail.com>
2020-09-28 14:20:13 -0400

    add earth year in secs and planet ages

--- spaceAge.js
+++ spaceAge.js
@@ -17,6 +17,20 @@
 // If you're wondering why Pluto didn't make the cut, go watch [this
 // youtube video](http://www.youtube.com/watch?v=Z_2gbGXzFbs).
 
+const plantAges = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1.0,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+ }
+ 
+ const earthYearInSecs =  31557600
+ 
+
 
 const space = (planets,ageSecs) => {
     return planets, ageSecs
