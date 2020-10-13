e0fe52d9f42b32f44878999ab2127d28180deb2e
Don-Stevenson <dsteven1@gmail.com>
2020-10-13 11:59:50 -0400

    code cleanup and update comments

--- resistor-colour.js
+++ resistor-colour.js
@@ -23,13 +23,15 @@
 // Mnemonics map the colors to the numbers, that, when stored as an array,
 // happen to map to their index in the array: Better Be Right Or Your Great Big Values Go Wrong.
 
-// More information on the color encoding of resistors can be found in the [Electronic color code Wikipedia article](https://en.wikipedia.org/wiki/Electronic_color_code)
+// More information on the color encoding of resistors can be found in the 
+// [Electronic color code Wikipedia article](https://en.wikipedia.org/wiki/Electronic_color_code)
 //
+
+//**********************************************************************************************/
 
 const colorCode = colour => {
 
-    // return COLORS.findIndex(colorCode(colour))
-  
+    
     const colourObj = {
       black: 0,
       brown: 1,
