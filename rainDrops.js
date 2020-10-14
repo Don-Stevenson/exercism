4f45896538506d0a577860fbf804a7c974183a2d
Don-Stevenson <dsteven1@gmail.com>
2020-10-14 16:13:45 -0400

    with ternary statements, working solution

--- rainDrops.js
+++ rainDrops.js
@@ -19,12 +19,9 @@
 // *****************************************************
 
 const convert = (num) => {
-  if (num % 3 === 0 && num % 5 === 0 && num % 7 === 0) return "PlingPlangPlong";
-  if (num % 3 === 0 && num % 5 === 0) return "PlingPlang";
-  if (num % 3 === 0 && num % 7 === 0) return "PlingPlong";
-  if (num % 5 === 0 && num % 7 === 0) return "PlangPlong";
-  if (num % 3 === 0) return "Pling";
-  if (num % 5 === 0) return "Plang";
-  if (num % 7 === 0) return "Plong";
-  else return num.toString();
+  const pling = num % 3 === 0 ? "Pling" : "";
+  const plang = num % 5 === 0 ? "Plang" : "";
+  const plong = num % 7 === 0 ? "Plong" : "";
+
+  return `${pling}${plang}${plong}` || `${num}`;
 };
