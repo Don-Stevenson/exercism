4873a8bac1ed761ff66380655fa42045be03ef0b
Don-Stevenson <dsteven1@gmail.com>
2020-10-17 12:29:12 -0400

    update comments

--- darts.js
+++ darts.js
@@ -24,8 +24,8 @@
 // coordinates `x` and `y`), 
 // returns the correct amount earned by a dart landing in that point.
 //
-
 //********************************************************************************************
+
 const score = (x, y) => {
   const dartLocationRadius = Math.sqrt( Math.pow((x), 2) + Math.pow((y), 2))
   if (dartLocationRadius > 10) return 0
