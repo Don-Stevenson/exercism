1784f06f6506c561c10d1c51bbe8072056015c1f
Don-Stevenson <dsteven1@gmail.com>
2020-10-20 13:16:51 -0400

    initial commit

---
+++ wordCount.js
@@ -0,0 +1,18 @@
+// # Word Count
+
+// Given a phrase, count the occurrences of each word in that phrase.
+
+// For example for the input `"olly olly in come free"`
+
+// ```text
+// olly: 2
+// in: 1
+// come: 1
+// free: 1
+// ```
+
+
+export const countWords = () => {
+    throw new Error("Remove this statement and implement this function");
+  };
+  
