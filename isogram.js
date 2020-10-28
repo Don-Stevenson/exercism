bc30cfd3d036fab5e8c137399fbbbe337f982a3b
Don-Stevenson <dsteven1@gmail.com>
2020-10-28 15:35:27 -0400

    refactor code

--- isogram.js
+++ isogram.js
@@ -14,27 +14,25 @@
 // The word *isograms*, however, is not an isogram, because the s repeats.
 //************************************************************************
 
-
 export const isIsogram = (word) => {
-    // const input = word.split("");
-    // console.log({ input });
-    // let letterObj = {};
-    // for (const letter of input) {
-    //   console.log(letter);
-    //   if (letterObj.hasOwnProperty(letter)) {
-    //     letterObj[letter] = letterObj += 1;
-    //     console.log("here");
-    //   } else letterObj[letter] = 1;
-    // }
-    // return letterObj.hasOwnProperty(2) ? false : true;
+  // const input = word.split("");
+  // console.log({ input });
+  // let letterObj = {};
+  // for (const letter of input) {
+  //   console.log(letter);
+  //   if (letterObj.hasOwnProperty(letter)) {
+  //     letterObj[letter] = letterObj += 1;
+  //     console.log("here");
+  //   } else letterObj[letter] = 1;
+  // }
+  // return letterObj.hasOwnProperty(2) ? false : true;
 
-    const lowerCased = word.toLowerCase();
+  const lowerCased = word.toLowerCase();
 
-    const result = lowerCased
-      .split("")
-      .every((v, i) => lowerCased.indexOf(v) === i);
-  
-    return result;
-  };
-  
+  const result = word
+    .toLowerCase()
+    .split("")
+    .every((v, i) => lowerCased.indexOf(v) === i);
 
+  return result;
+};
