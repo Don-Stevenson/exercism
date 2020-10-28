558549f397ed2e2541a98b0e8545228cbb4470ec
Don-Stevenson <dsteven1@gmail.com>
2020-10-28 13:45:20 -0400

    update comments

--- leap.js
+++ leap.js
@@ -28,7 +28,7 @@
 //3 If the year is evenly divisible by 400, go to step 4. Otherwise, go to step 5.
 //4 The year is a leap year (it has 366 days).
 //5 The year is not a leap year (it has 365 days).
-// ******************************************************************************
+// *******************************************************************************
 
 const isLeap = year =>
   (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
