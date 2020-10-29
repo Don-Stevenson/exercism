5add3f99fb9424ecca2f264fb274e03923750bce
Don-Stevenson <dsteven1@gmail.com>
2020-10-29 10:22:38 -0400

    reformat code

--- reverse-string.js
+++ reverse-string.js
@@ -6,8 +6,9 @@
 // const string = "hello";
 // console.log(string.substr(1), string.charAt(0));
 
-//**************************************************
+//*************************************************
 
-const reverseString = str => (str === "") ? "" : reverseString(str.substr(1)) + str.charAt(0);
+const reverseString = (str) =>
+  str === "" ? "" : reverseString(str.substr(1)) + str.charAt(0);
 
 console.log(reverseString("Here"));
