e23901f606ca54d35a175130d012070628350d59
Don-Stevenson <dsteven1@gmail.com>
2020-10-29 10:22:09 -0400

    reformat code

--- spaceAge.js
+++ spaceAge.js
@@ -20,16 +20,17 @@
 //*************************************************************************
 
 const plantAges = {
-    mercury: 0.2408467,
-    venus: 0.61519726,
-    earth: 1.0,
-    mars: 1.8808158,
-    jupiter: 11.862615,
-    saturn: 29.447498,
-    uranus: 84.016846,
-    neptune: 164.79132
- }
- 
- const earthYearInSecs =  31557600
- 
- const age = (planet, ageInSeconds) => parseFloat((ageInSeconds / (earthYearInSecs * plantAges[planet])).toFixed(2))
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1.0,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
+
+const earthYearInSecs = 31557600;
+
+const age = (planet, ageInSeconds) =>
+  parseFloat((ageInSeconds / (earthYearInSecs * plantAges[planet])).toFixed(2));
