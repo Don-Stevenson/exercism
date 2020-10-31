be20be1dd87e123111866c7b986d122139d8caa8
Don-Stevenson <dsteven1@gmail.com>
2020-10-31 18:41:34 -0400

    update comments

--- reverse-string.js
+++ reverse-string.js
@@ -6,7 +6,7 @@
 // const string = "hello";
 // console.log(string.substr(1), string.charAt(0));
 
-//*************************************************
+//**************************************************
 
 const reverseString = (str) =>
   str === "" ? "" : reverseString(str.substr(1)) + str.charAt(0);
