28ada7a543f1a19862988bece3a21cf8a6d55f1e
Don-Stevenson <dsteven1@gmail.com>
2020-11-01 11:49:35 -0500

    reformat code

--- hamming.js
+++ hamming.js
@@ -7,8 +7,8 @@
 // In fact, the average human body experiences about 10 quadrillion cell divisions in a lifetime!
 
 // When cells divide, their DNA replicates too.
-// Sometimes during this process mistakes happen and single pieces of DNA get encoded with 
-// the incorrect information. If we compare two strands of DNA and count the differences between 
+// Sometimes during this process mistakes happen and single pieces of DNA get encoded with
+// the incorrect information. If we compare two strands of DNA and count the differences between
 // them we can see how many mistakes occurred. This is known as the "Hamming Distance".
 
 // We read DNA using the letters C,A,G and T. Two strands might look like this:
@@ -19,17 +19,18 @@
 
 // They have 7 differences, and therefore the Hamming Distance is 7.
 
-// The Hamming Distance is useful for lots of things in science, not just biology, 
+// The Hamming Distance is useful for lots of things in science, not just biology,
 //so it's a nice phrase to be familiar with :)
 //********************************************************************************
 
 const compute = (leftStrand, rightStrand) => {
-    if (leftStrand.length === 0 && rightStrand.length === 0) return 0
-    if (leftStrand.length === 0) throw new Error('left strand must not be empty')
-    if (rightStrand.length === 0) throw new Error('right strand must not be empty')
-    if (rightStrand.length !== leftStrand.length) throw new Error('left and right strands must be of equal length')
-    return leftStrand.split("")
-      .filter((current, index) => current !== rightStrand[index])
-      .length
-  };
-  
+  if (leftStrand.length === 0 && rightStrand.length === 0) return 0;
+  if (leftStrand.length === 0) throw new Error("left strand must not be empty");
+  if (rightStrand.length === 0)
+    throw new Error("right strand must not be empty");
+  if (rightStrand.length !== leftStrand.length)
+    throw new Error("left and right strands must be of equal length");
+  return leftStrand
+    .split("")
+    .filter((current, index) => current !== rightStrand[index]).length;
+};
