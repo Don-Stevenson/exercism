d01094389727cf44a45b5530c8984946d7e88a86
Don-Stevenson <dsteven1@gmail.com>
2020-11-01 11:50:10 -0500

    reformat code

--- lessThanGreaterThan.js
+++ lessThanGreaterThan.js
@@ -1,11 +1,10 @@
 // testing out if statements between two values
 // ********************************************
 
-
 const between = (x) => {
-    if (x >= 1 && x < 8 ) return "between 1 and 8" 
-    else if (x >= 8 && x < 20) return "between 8 and 20"
-    else return "not between"
-}
+  if (x >= 1 && x < 8) return "between 1 and 8";
+  else if (x >= 8 && x < 20) return "between 8 and 20";
+  else return "not between";
+};
 
-console.log(between(1))
+console.log(between(1));
