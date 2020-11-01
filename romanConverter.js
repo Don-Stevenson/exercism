748d72956fdbd224f960380d6b0383fecbcf0e45
Don-Stevenson <dsteven1@gmail.com>
2020-11-01 11:57:03 -0500

    reformat code

--- romanConverter.js
+++ romanConverter.js
@@ -2,32 +2,31 @@
 // *********************************************************************************
 
 const romanNums = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1 
-  }
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
 
-  // function that returns a roman numeral given an arabic number
-  // *************************************************************
-  
-  const toRoman = (num) => {
-    let romanNum = "";
-    for (const index in romanNums) {
-       while (num >= romanNums[index]) {
-        romanNum += index;
-        num -= romanNums[index];
-      }
+// function that returns a roman numeral given an arabic number
+// *************************************************************
+
+const toRoman = (num) => {
+  let romanNum = "";
+  for (const index in romanNums) {
+    while (num >= romanNums[index]) {
+      romanNum += index;
+      num -= romanNums[index];
     }
-    return romanNum;
-  };
-  
+  }
+  return romanNum;
+};
