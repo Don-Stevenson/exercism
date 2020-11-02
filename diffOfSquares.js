c76bf1678cc3cd996e986ca13c859d583b7b1e41
Don-Stevenson <dsteven1@gmail.com>
2020-11-02 10:34:02 -0500

    with class and default errors

--- diffOfSquares.js
+++ diffOfSquares.js
@@ -1,1 +1,18 @@
-const diffSquares =() => {}
+export class Squares {
+    constructor() {
+      throw new Error("Remove this statement and implement this function");
+    }
+  
+    get sumOfSquares() {
+      throw new Error("Remove this statement and implement this function");
+    }
+  
+    get squareOfSum() {
+      throw new Error("Remove this statement and implement this function");
+    }
+  
+    get difference() {
+      throw new Error("Remove this statement and implement this function");
+    }
+  }
+  
