6f8fe24e713f5ba5bf3d5a9ddf5b3bc44c644e8e
Don-Stevenson <dsteven1@gmail.com>
2020-11-02 17:22:06 -0500

    setting up number constructor and console logging it

--- diffOfSquares.js
+++ diffOfSquares.js
@@ -1,18 +1,35 @@
+// # Difference Of Squares
+
+// Find the difference between the square of the sum and the sum of the squares of the first N natural numbers.
+
+// The square of the sum of the first ten natural numbers is
+// (1 + 2 + ... + 10)² = 55² = 3025.
+
+// The sum of the squares of the first ten natural numbers is
+// 1² + 2² + ... + 10² = 385.
+
+// Hence the difference between the square of the sum of the first
+// ten natural numbers and the sum of the squares of the first ten
+// natural numbers is 3025 - 385 = 2640.
+
+// You are not expected to discover an efficient solution to this yourself from
+// first principles; research is allowed, indeed, encouraged. Finding the best
+// algorithm for the problem is a key skill in software engineering.
+
 export class Squares {
-    constructor() {
-      throw new Error("Remove this statement and implement this function");
-    }
-  
-    get sumOfSquares() {
-      throw new Error("Remove this statement and implement this function");
-    }
-  
-    get squareOfSum() {
-      throw new Error("Remove this statement and implement this function");
-    }
-  
-    get difference() {
-      throw new Error("Remove this statement and implement this function");
-    }
+  constructor(num) {
+   this.num = num;
   }
-  
+
+  get sumOfSquares() {
+     console.log({num});
+  }
+
+  get squareOfSum() {
+ console.log("num =>", this.num)
+  }
+
+  get difference() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
