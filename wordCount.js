0b5fa3feacf6c6d04d01906a31139b0b56279e27
Don-Stevenson <dsteven1@gmail.com>
2020-11-05 21:05:51 -0500

    update comments

--- wordCount.js
+++ wordCount.js
@@ -1,15 +1,15 @@
 // # Word Count
+// ************
 
 // Given a phrase, count the occurrences of each word in that phrase.
-
 // For example for the input `"olly olly in come free"`
-
 // ```text
 // olly: 2
 // in: 1
 // come: 1
 // free: 1
 // ```
+// *******************************************************************
 
 const countWords = (sentence) => {
   const wordsArr = sentence.toLowerCase().match(/\b\w+('\w+)?/g);
