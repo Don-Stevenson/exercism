35d31738f83120f88f620bb47d83b638e9448e40
Don-Stevenson <dsteven1@gmail.com>
2020-11-09 12:41:51 -0500

    update comments

--- nucleoTideCounts.js
+++ nucleoTideCounts.js
@@ -11,7 +11,7 @@
 // - nucleotides are to DNA as
 // - legos are to lego houses as
 // - words are to sentences as...
-// *******************************
+// ******************************
 
 
 export class NucleotideCounts {
