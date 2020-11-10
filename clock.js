488b9c073614a809b127ffdc685e2c3cfd195605
Don-Stevenson <dsteven1@gmail.com>
2020-11-10 14:37:18 -0500

    initial commit

---
+++ clock.js
@@ -0,0 +1,29 @@
+// # Clock
+
+// Implement a clock that handles times without dates.
+
+// You should be able to add and subtract minutes to it.
+
+// Two clocks that represent the same time should be equal to each other.
+
+export class Clock {
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  toString() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  plus() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  minus() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  equals() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
