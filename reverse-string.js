65e2cc9bb17ae1578ee9318a07ca509b4521cf20
Don-Stevenson <dsteven1@gmail.com>
2020-11-10 14:38:50 -0500

    update comments

--- reverse-string.js
+++ reverse-string.js
@@ -5,8 +5,7 @@
 // output: "looc"
 // const string = "hello";
 // console.log(string.substr(1), string.charAt(0));
-
-//*************************************************
+//**************************************************
 
 const reverseString = (str) =>
   str === "" ? "" : reverseString(str.substr(1)) + str.charAt(0);
