410ad30575a1eaa841b6d73bcf5beb55a8893a43
Don-Stevenson <dsteven1@gmail.com>
2020-11-11 17:27:48 -0500

    remove else and add modulo for hour greater than 24. passing 4 tests

--- clock.js
+++ clock.js
@@ -24,11 +24,11 @@ export class Clock {
     let hour = this.hour;
     let min = this.min;
     console.log("hour + min before", hour, min);
-    if (hour >= 24) hour = hour - 24;
-    else if (min < 10) min = `0${min}`;
-    else if (!min) min = "00";
-    else if (hour < 10) hour = `0${hour}`;
-    else if (!hour || hour === 24) hour = "00";
+    if (hour >= 24) hour = hour % 24;
+    if (min < 10) min = `0${min}`;
+    if (!min) min = "00";
+    if (hour < 10) hour = `0${hour}`;
+    if (!hour || hour === 24) hour = "00";
     console.log("after", { hour }, { min });
 
     return `${hour}:${min}`;
