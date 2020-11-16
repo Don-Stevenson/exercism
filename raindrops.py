da1b0045e489a950c2cc476f5e66aa00cd41b00d
Don-Stevenson <dsteven1@gmail.com>
2020-11-15 22:45:12 -0500

    passing all tests

---
+++ raindrops.py
@@ -0,0 +1,22 @@
+
+# Raindrops
+# check if a given number is divisable by 3, 5, 7 and return Pling, Plang or Plong
+# if number is not divisable by 3, 5, or 7, return the number
+# ********************************************************************************
+
+def convert(number):
+    pling = ""
+    plang = ""
+    plong = ""
+
+    if number % 3 != 0 and number % 5 != 0 and number % 7 != 0:
+        return str(number)
+
+    if number % 3 == 0:
+        pling = 'Pling'
+    if number % 5 == 0:
+        plang = 'Plang'
+    if number % 7 == 0:
+        plong = 'Plong'
+
+    return pling+plang+plong
