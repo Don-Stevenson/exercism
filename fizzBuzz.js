d912654b17ff1fdc7d89f65a8c30c71304c179f2
Don-Stevenson <dsteven1@gmail.com>
2020-12-14 14:23:17 -0500

    add instructions and function set up

---
+++ fizzBuzz.js
@@ -0,0 +1,13 @@
+// prints numbers 1 to 100
+// if number is muliple of 3 print fizz
+// if number is muliple of 5 print buzz
+// // if number is muliple of 3 and 5 print fizzbuzz
+// *************************************************
+
+
+const fizzBuzz = () => {
+  
+  };
+  
+  fizzBuzz();
+  
