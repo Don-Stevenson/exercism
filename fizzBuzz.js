e67648daf5b470e7175142a91a008c40f31f9215
Don-Stevenson <dsteven1@gmail.com>
2020-12-14 14:24:09 -0500

    working solution

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,13 +1,16 @@
 // prints numbers 1 to 100
 // if number is muliple of 3 print fizz
 // if number is muliple of 5 print buzz
-// // if number is muliple of 3 and 5 print fizzbuzz
+// if number is muliple of 3 and 5 print fizzbuzz
 // *************************************************
 
-
 const fizzBuzz = () => {
-  
-  };
-  
-  fizzBuzz();
-  
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) console.log("fizzbuzz");
+    else if (i % 3 === 0) console.log("fizz");
+    else if (i % 5 === 0) console.log("buzz");
+    else console.log(i);
+  }
+};
+
+fizzBuzz();
