98073dbd5f0f4784a525aedb1a701b0a17b2071a
Don-Stevenson <dsteven1@gmail.com>
2020-12-17 15:37:15 -0500

    update comments

--- armstrongNums.js
+++ armstrongNums.js
@@ -2,7 +2,6 @@
 // raised to the power of the number of digits.
 
 // For example:
-
 // - 9 is an Armstrong number, because `9 = 9^1 = 9
 // - 10 is *not* an Armstrong number, because `10 != 1^2 + 0^2 = 1
 // - 153 is an Armstrong number, because: `153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
