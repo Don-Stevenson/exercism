f5c9f546f9e38cfb36cb2c8e351a794c7486aec7
Don-Stevenson <dsteven1@gmail.com>
2020-12-17 15:36:19 -0500

    update comments

--- darts.js
+++ darts.js
@@ -1,10 +1,8 @@
 // # Darts
 
 // Write a function that returns the earned points in a single toss of a Darts game.
-
 // [Darts](https://en.wikipedia.org/wiki/Darts) is a game where players
 // throw darts to a [target](https://en.wikipedia.org/wiki/Darts#/media/File:Darts_in_a_dartboard.jpg).
-
 // In our particular instance of the game, the target rewards with 4 different amounts of points,
 // depending on where the dart lands:
 
