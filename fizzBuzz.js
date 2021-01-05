1d0573488becd915e68309c1d4d78e7a0cbad141
Don-Stevenson <dsteven1@gmail.com>
2021-01-05 13:04:35 -0500

    update comments

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,8 +1,11 @@
+// #fizzbuzz
+// *********
+
 // prints numbers 1 to 100
 // if number is muliple of 3 print fizz
 // if number is muliple of 5 print buzz
 // if number is muliple of 3 and 5 print fizzbuzz
-// *************************************************
+// **********************************************
 
 const fizzBuzz = () => {
   for (let i = 1; i <= 100; i++) {
