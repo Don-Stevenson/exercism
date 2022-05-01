b2ecf9a1030e1bd8b5586f62ca2abda69eb94bba
Don Stevenson <dsteven1@gmail.com>
2022-04-30 20:00:54 -0400

    fix object to contain lowercase numbers, passing solution

--- TS/resister-color-duo.ts
+++ TS/resister-color-duo.ts
@@ -1,25 +1,19 @@
-interface Colour {
-  [c: string]: number;
-}
+interface Colour { [colour: string]: number }
 
 const resisterObj: Colour = {
-  Black: 0,
-  Brown: 1,
-  Red: 2,
-  Orange: 3,
-  Yellow: 4,
-  Green: 5,
-  Blue: 6,
-  Violet: 7,
-  Grey: 8,
-  White: 9,
-};
+black: 0,
+brown: 1,
+red: 2,
+orange: 3,
+yellow: 4,
+green: 5,
+blue: 6,
+violet: 7,
+grey: 8,
+white: 9,
+}
 
 export function decodedValue(colours: string[]): number {
-  if (colours.length < 2) {
-    throw "At least two colors need to be present";
-  }
-  const [firstColour, secondColour] = colours;
-  console.log(`${resisterObj[firstColour]}${resisterObj[secondColour]}`);
-  return Number(`${resisterObj[firstColour]}${resisterObj[secondColour]}`);
+  const [firstColour, secondColour] = colours
+  return Number(`${resisterObj[firstColour]}${resisterObj[secondColour]}`)
 }
