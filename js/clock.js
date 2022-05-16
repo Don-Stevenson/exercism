6433aec8df6266d6a084693aea7c762d9312aba4
Don Stevenson <dsteven1@gmail.com>
2022-05-16 14:32:12 -0400

    Update formatting

--- js/clock.js
+++ js/clock.js
@@ -12,33 +12,33 @@
 
 export class Clock {
   constructor(hour, min) {
-    this.hour = hour;
-    this.min = min;
+    this.hour = hour
+    this.min = min
   }
 
   toString() {
-    let hour = this.hour;
-    let min = this.min;
-    console.log("hour + min before", hour, min);
-    if (hour >= 24) hour = hour % 24;
-    if (min < 10) min = `0${min}`;
-    if (!min) min = "00";
-    if (hour < 10) hour = `0${hour}`;
-    if (!hour || hour === 24) hour = "00";
-    console.log("after", { hour }, { min });
+    let hour = this.hour
+    let min = this.min
+    console.log("hour + min before", hour, min)
+    if (hour >= 24) hour = hour % 24
+    if (min < 10) min = `0${min}`
+    if (!min) min = "00"
+    if (hour < 10) hour = `0${hour}`
+    if (!hour || hour === 24) hour = "00"
+    console.log("after", { hour }, { min })
 
-    return `${hour}:${min}`;
+    return `${hour}:${min}`
   }
 
   plus() {
-    throw new Error("Remove this statement and implement this function");
+    throw new Error("Remove this statement and implement this function")
   }
 
   minus() {
-    throw new Error("Remove this statement and implement this function");
+    throw new Error("Remove this statement and implement this function")
   }
 
   equals() {
-    throw new Error("Remove this statement and implement this function");
+    throw new Error("Remove this statement and implement this function")
   }
 }
