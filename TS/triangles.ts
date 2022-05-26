c6784aba994c278aa7f1fde1c6329d7062b239c0
Don Stevenson <dsteven1@gmail.com>
2022-05-25 23:41:25 -0400

    Add working solution

--- TS/triangles.ts
+++ TS/triangles.ts
@@ -1,39 +1,52 @@
 export class Triangle {
-    private sides: number[]
-  
-      constructor(...sides: number[]) {
-      this.sides = sides
-    }
-  
-    get isEquilateral() {
-      return isTriangle(this.sides[0], this.sides[1], this.sides[2]) &&
-        this.sides[0] * this.sides[1] * this.sides[2] ===
-          Math.pow(this.sides[0], 3) &&
-        areTrianglesSidesAllZero(this.sides[0], this.sides[1], this.sides[2])
-        ? true
-        : false
-    }
-  
-    get isIsosceles() {
-      return (isTriangle(this.sides[0], this.sides[1], this.sides[2]) &&
-        this.sides[0] === this.sides[1]) ||
-        this.sides[0] === this.sides[2] ||
-        (this.sides[1] === this.sides[2] &&
-          areTrianglesSidesAllZero(this.sides[0], this.sides[1], this.sides[2]))
-        ? true
-        : false
-    }
-  
-    get isScalene() {
-      throw new Error("Remove this statement and implement this function")
-    }
+  private a: number
+  private b: number
+  private c: number
+
+  constructor(...sides: number[]) {
+    this.a = sides[0]
+    this.b = sides[1]
+    this.c = sides[2]
   }
-  
-  const isTriangle = (a: number, b: number, c: number): boolean =>
-    a <= b + c && b <= a + c && c <= b + a
-  
-  const areTrianglesSidesAllZero = (a: number, b: number, c: number): boolean =>
-    a !== 0 && b !== 0 && c !== 0
-  
-  console.log("here", isTriangle(1, 3, 1))
+
+  get isEquilateral() :boolean {
+    return isTriangle(this.a, this.b, this.c) &&
+      areTrianglesSidesAllZero(this.a, this.b, this.c) &&
+      checkIfEquilateralTriangle(this.a, this.b, this.c)
+      ? true
+      : false
+  }
+
+  get isIsosceles() :boolean {
+    return isTriangle(this.a, this.b, this.c) &&
+      areTrianglesSidesAllZero(this.a, this.b, this.c) &&
+      checkIfIsoscelesTriangle(this.a, this.b, this.c)
+      ? true
+      : false
+  }
+
+  get isScalene() :boolean {
+    return isTriangle(this.a, this.b, this.c) &&
+      areTrianglesSidesAllZero(this.a, this.b, this.c) &&
+      checkIfScaleneTriangle(this.a, this.b, this.c)
+      ? true
+      : false
+  }
+}
+
+const isTriangle = (a: number, b: number, c: number): boolean =>
+  a <= b + c && b <= a + c && c <= b + a
+
+const areTrianglesSidesAllZero = (a: number, b: number, c: number): boolean =>
+  a !== 0 && b !== 0 && c !== 0
+
+const checkIfEquilateralTriangle = (a: number, b: number, c: number): boolean =>
+  a * b * c === Math.pow(a, 3)
+
+const checkIfIsoscelesTriangle = (a: number, b: number, c: number): boolean =>
+  a === b || a === c || b === c
+
+const checkIfScaleneTriangle = (a: number, b: number, c: number): boolean =>
+  !(a === b || a === c || b === c)
+
   
